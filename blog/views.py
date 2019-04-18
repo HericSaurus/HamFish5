f93d94ce2792653f9f395469bc0f238dfe94cd23
HericSaurus <bch_heric@hotmail.com>
2019-04-18 18:45:55 +0800

    Added Models

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'HericB',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 21,2018'
-    },
-
-    {
-        'author': 'BaconHamfish',
-        'title': 'Blog Post 2',
-        'content': 'Fishy Content',
-        'date_posted': 'August 29,2018'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
